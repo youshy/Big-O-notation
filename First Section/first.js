bedc633dc4d57354db8edeb2993f96fe6a3c8cc9
Artur <devilyoushy@gmail.com>
2018-11-22 15:07:52 +0000

    algorithm test

--- First Section/first.js
+++ First Section/first.js
@@ -1,9 +1,25 @@
 const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "gill",
+  "nemo",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank"
+];
+const large = new Array(100).fill("nemo");
 
 function findNemo(array) {
+  let t0 = performance.now();
   for (let i = 0; i < array.length; i++) {
     if (array[i] === "nemo") [console.log("FOUND NEMO!")];
   }
+  let t1 = performance.now();
+  console.log(`Call to find Nemo took ${t1 - t0} milliseconds`);
 }
 
-findNemo(nemo);
+findNemo(large);
