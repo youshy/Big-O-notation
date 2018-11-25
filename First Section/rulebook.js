2696ca3891fb881125d2a5cdf090c6057feb1143
Artur <devilyoushy@gmail.com>
2018-11-25 12:14:06 +0000

    Worst case

--- First Section/rulebook.js
+++ First Section/rulebook.js
@@ -1,6 +1,36 @@
 // Rule book for simplyfying Big O notation
 
 // 1. Worst case
+
+// Making loop more efficient
+// Based on Finding Nemo
+
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "gill",
+  "nemo",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank"
+];
+
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === "nemo") {
+      console.log("FOUND NEMO!");
+      break; // Loop will break when Nemo found
+    }
+  }
+}
+
+// But what if Nemo is the last one? Worst case scenario - based on that we can assume Big O notation - if Nemo will be last element of an array - O(n)
+
+findNemo(everyone);
+
 // 2. Remove Constants
 // 3. Different terms for inputs
 // 4. Drop non dominants
