6958878fc061136a3a24ffe7216b55272e3ebcd2
Artur <devilyoushy@gmail.com>
2018-11-26 05:25:16 +0000

    Space Complexity

---
+++ First Section/notes.js
@@ -0,0 +1,32 @@
+// 3 Pillars Of Programming
+
+// Readable
+// Speed - Time Complexity
+// Memory - Space Complexity
+
+// Measuring Space Complexity:
+
+// Heap - where we store variables
+// Stack - where we store function calls
+
+// What causes Space Complexity?
+
+// Variables
+// Data Structures
+// Function Call
+// Allocation
+
+// Space Complexity problem example:
+function boooo(n) {
+  for (let i = 0; i < n.length; i++) console.log("boo!");
+}
+
+boooo([1, 2, 3, 4, 5, 6]); // Space Complexity of O(1) - we're just assigning memory to create variable "i"
+
+function booArray(n) {
+  let booArray = [];
+  for (let i = 0; i < 6; i++) booArray[i] = "Booo!";
+  return booArray;
+}
+
+booArray(); // Space Complexity of O(n) - because we've created data structure
