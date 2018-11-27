987ad928fe7d6be190023c0ff23d74c433044072
Artur <devilyoushy@gmail.com>
2018-11-27 06:12:05 +0000

    Data Structures: Static vs Dynamic arrays and implementation in JavaScript

--- Data Structures/Arrays/notes.js
+++ Data Structures/Arrays/notes.js
@@ -17,3 +17,9 @@ strings.splice(2, 0, "wait, what?"); // O(n) - because it has to reassing indexe
 
 // Final version of the array
 console.log(strings);
+
+// Static vs dynamic arrays
+
+// Static array has memory allocated from start - we define how big the array should be and with expansion what we do is that we copy the array again and then allocate same amount of memory to it
+// Dynamic array has memory allocated for given amount of members - so, it expands easily
+// JavaScript doesn't have static arrays - every array will be dynamic from start
