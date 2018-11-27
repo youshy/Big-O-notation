324b167ba9221575b0ed4b00451e82d0414a3777
Artur <devilyoushy@gmail.com>
2018-11-27 05:32:03 +0000

    Data Structures: Notes & Intro

---
+++ Data Structures/notes.js
@@ -0,0 +1,6 @@
+// Data Structure is a collection of values
+// Every structure is specialized in some sort
+
+// Cache - very fast memory, loaded from RAM
+
+// JavaScript doesn't really have integers, it uses 64-bit floats - so, it has maximum biggest value
