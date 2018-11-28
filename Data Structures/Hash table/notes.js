e97a0635c7ee684cea688d03339d2529305ff186
Artur <devilyoushy@gmail.com>
2018-11-28 05:38:13 +0000

    Hash Tables: hash functions

--- Data Structures/Hash table/notes.js
+++ Data Structures/Hash table/notes.js
@@ -1,2 +1,8 @@
 // Hash maps, hash tables, dictionaries, objects - same name
 // Object in JavaScript is a Hash Table
+
+// Has key-value pairs
+
+// Idempotent - function, given an input, always gives an output
+
+// Implementation of hash functions - MD5, SHA-1, SHA-256
