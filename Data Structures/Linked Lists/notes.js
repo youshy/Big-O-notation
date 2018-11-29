d8ad10ce1b504d07c64e4f34e86c725f9c35f01a
Artur <devilyoushy@gmail.com>
2018-11-29 05:47:07 +0000

    Linked Lists: finished constructor

--- Data Structures/Linked Lists/notes.js
+++ Data Structures/Linked Lists/notes.js
@@ -39,4 +39,28 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
+  }
+
+  prepend(value) {
+    const newNode = {
+      value: value,
+      next: null
+    };
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+    return this;
+  }
 }
+
+const myList = new LinkedList(10);
