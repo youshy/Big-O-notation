bb5e2f8d3dd1cceb59550b21a86dd9bf3c6bdf0e
Artur <devilyoushy@gmail.com>
2018-11-29 05:18:49 +0000

    Linked Lists: notes

---
+++ Data Structures/Linked Lists/notes.js
@@ -0,0 +1,9 @@
+//
+// What is a linked list?
+//
+
+// Built by two linked nodes - head & tail (last node)
+// It's null-terminated - last part will point to null
+// Every node will point to next node
+
+// JavaScript doesn't have pre-built linked lists data structure
